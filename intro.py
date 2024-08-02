638cb797ee295429ea47dc3b10119547769c1de5
sayeedarefin <sayeedarefin5@gmail.com>
2024-08-02 10:34:12 +0600

    first commit

---
+++ intro.py
@@ -0,0 +1,13 @@
+if 5 > 2:
+ print("Five is greater than two!")
+ print("Five is greater than two!")
+ #this is a comment
+ ###########################################
+ ########### we declare variable direcly#############
+
+ x=5
+ y=2
+ z="Hello"
+ """
+ this is a multi line comment
+ """
